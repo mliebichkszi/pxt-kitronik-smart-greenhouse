114fd763fa91f0d92d84b6ae22bfb8ab62f223e7
mliebichkszi <72981023+mliebichkszi@users.noreply.github.com>
2021-12-12 00:16:02 +0100

    Aktualisiere main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 bodenfeuchtigkeit = 0
 luftfeuchtigkeit = 0
 temperatur = 0
-zipLEDs = kitronik_smart_greenhouse.create_greenhouse_zip_display(8)
+kitronik_smart_greenhouse.set_buzzer_pin()
+zipLEDs = kitronik_smart_greenhouse.create_greenhouse_zip_display(3)
 statusLEDs = zipLEDs.status_leds_range()
 
 def on_forever():
@@ -36,14 +37,20 @@ def on_forever():
     statusLEDs.set_zip_led_color(2, kitronik_smart_greenhouse.hue_to_rgb(bodenfeuchtigkeit))
     statusLEDs.show()
     if kitronik_smart_greenhouse.read_io_pin(kitronik_smart_greenhouse.PinType.ANALOG,
-        kitronik_smart_greenhouse.IOPins.P1) <= 400:
+        kitronik_smart_greenhouse.IOPins.P1) <= 100:
         basic.show_icon(IconNames.SAD)
         music.start_melody(music.built_in_melody(Melodies.BA_DING),
             MelodyOptions.FOREVER)
-        basic.pause(100)
+        basic.pause(2000)
         music.stop_melody(MelodyStopOptions.ALL)
-        for index in range(3):
-            pass
+        for index in range(1):
+            kitronik_smart_greenhouse.control_high_power_pin(kitronik_smart_greenhouse.HighPowerPins.PIN13,
+                kitronik_smart_greenhouse.on_off(True))
+            basic.pause(1000)
+            kitronik_smart_greenhouse.control_high_power_pin(kitronik_smart_greenhouse.HighPowerPins.PIN13,
+                kitronik_smart_greenhouse.on_off(False))
+            basic.pause(2000)
     else:
         basic.show_icon(IconNames.HAPPY)
+    basic.pause(10000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,39 +1,74 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(kitronik_smart_greenhouse.temperature(TemperatureUnitList.C))
-})
-input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(kitronik_smart_greenhouse.humidity())
     basic.showNumber(kitronik_smart_greenhouse.readIOPin(kitronik_smart_greenhouse.PinType.analog, kitronik_smart_greenhouse.IOPins.p1))
 })
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(kitronik_smart_greenhouse.humidity())
+input.onButtonPressed(Button.AB, function () {
+    if (farbe == 5) {
+        farbe = 0
+    } else {
+        farbe += 1
+    }
 })
 let bodenfeuchtigkeit = 0
 let luftfeuchtigkeit = 0
 let temperatur = 0
+let farbe = 0
 kitronik_smart_greenhouse.setBuzzerPin()
+let zipLEDs2 = kitronik_smart_greenhouse.createGreenhouseZIPDisplay(8)
+let zipStick = zipLEDs2.zipStickRange()
 let zipLEDs = kitronik_smart_greenhouse.createGreenhouseZIPDisplay(3)
 let statusLEDs = zipLEDs.statusLedsRange()
+farbe = 0
 basic.forever(function () {
     temperatur = Math.map(kitronik_smart_greenhouse.temperature(TemperatureUnitList.C), 0, 40, 210, 0)
     luftfeuchtigkeit = Math.map(kitronik_smart_greenhouse.humidity(), 0, 100, 35, 150)
-    bodenfeuchtigkeit = Math.map(kitronik_smart_greenhouse.readIOPin(kitronik_smart_greenhouse.PinType.analog, kitronik_smart_greenhouse.IOPins.p1), 0, 1023, 35, 150)
+    bodenfeuchtigkeit = Math.map(kitronik_smart_greenhouse.readIOPin(kitronik_smart_greenhouse.PinType.analog, kitronik_smart_greenhouse.IOPins.p1), 0, 1023, 35, 500)
     statusLEDs.setZipLedColor(0, kitronik_smart_greenhouse.hueToRGB(temperatur))
     statusLEDs.setZipLedColor(1, kitronik_smart_greenhouse.hueToRGB(luftfeuchtigkeit))
     statusLEDs.setZipLedColor(2, kitronik_smart_greenhouse.hueToRGB(bodenfeuchtigkeit))
     statusLEDs.show()
-    if (kitronik_smart_greenhouse.readIOPin(kitronik_smart_greenhouse.PinType.analog, kitronik_smart_greenhouse.IOPins.p1) <= 400) {
-        basic.showIcon(IconNames.Sad)
+    if (kitronik_smart_greenhouse.readIOPin(kitronik_smart_greenhouse.PinType.analog, kitronik_smart_greenhouse.IOPins.p1) <= 50) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
         music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Forever)
         basic.pause(2000)
         music.stopMelody(MelodyStopOptions.All)
-        for (let index = 0; index < 3; index++) {
+        for (let index = 0; index < 1; index++) {
             kitronik_smart_greenhouse.controlHighPowerPin(kitronik_smart_greenhouse.HighPowerPins.pin13, kitronik_smart_greenhouse.onOff(true))
-            basic.pause(1000)
+            basic.pause(100)
             kitronik_smart_greenhouse.controlHighPowerPin(kitronik_smart_greenhouse.HighPowerPins.pin13, kitronik_smart_greenhouse.onOff(false))
             basic.pause(2000)
         }
     } else {
-        basic.showIcon(IconNames.Happy)
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
     }
-    basic.pause(10000)
+    basic.pause(50000)
+})
+basic.forever(function () {
+    if (farbe == 0) {
+        zipStick.showColor(kitronik_smart_greenhouse.colors(ZipLedColors.White))
+    } else if (farbe == 1) {
+        zipStick.showColor(kitronik_smart_greenhouse.colors(ZipLedColors.Red))
+    } else if (farbe == 2) {
+        zipStick.showColor(kitronik_smart_greenhouse.colors(ZipLedColors.Green))
+    } else if (farbe == 3) {
+        zipStick.showColor(kitronik_smart_greenhouse.colors(ZipLedColors.Blue))
+    } else if (farbe == 4) {
+        zipStick.showColor(kitronik_smart_greenhouse.rgb(220, 75, 200))
+    } else if (farbe == 5) {
+        zipLEDs.clear()
+        zipLEDs.show()
+    }
 })
